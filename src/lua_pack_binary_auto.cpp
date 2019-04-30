dfa86d4b351e1955a4a296d79eecd7d6085f0ef8
dingdekai <dekai.ding@shen021.com>
2019-04-30 16:45:30 +0800

    [Change] the path of head file.

--- src/lua_pack_binary_auto.cpp
+++ src/lua_pack_binary_auto.cpp
@@ -1,4 +1,4 @@
-#include "scripting/lua-bindings/auto/lua_pack_binary_auto.hpp"
+#include "lua_pack_binary_auto.hpp"
 #include "Binary.h"
 #include "scripting/lua-bindings/manual/tolua_fix.h"
 #include "scripting/lua-bindings/manual/LuaBasicConversions.h"
